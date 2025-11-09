e198b13006b17a06d6ec50b96c306ece5f51da60
combizera <ycombi349@gmail.com>
2025-11-09 18:02:09 -0300

    feat(LoginRequest): create form request and improve show error

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:60',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'O campo e-mail é obrigatório.',
+            'email.email' => 'O campo e-mail deve ser um endereço de e-mail válido.',
+            'password.required' => 'O campo senha é obrigatório.',
+            'password.min' => 'A senha deve ter no mínimo 6 caracteres.',
+            'password.max' => 'A senha deve ter no máximo 60 caracteres.',
+        ];
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,32 +1,53 @@
 <x-layout>
   <main class="py-10">
-    <h1>
-      Faça Login
-    </h1>
+     <section class="bg-white max-w-[600px] mx-auto p-10 border-2 mt-4">
 
-    <section class="mt-4">
-      <form action="/login" method="POST">
+       <h1 class="font-bold text-3xl">
+         Faça Login
+       </h1>
+
+       <p>
+         Insira seus dados para acessar
+       </p>
+
+      <form action="/login" method="POST" class="flex flex-col">
         @csrf
 
-        @error('email')
-          <p class="text-red-500 text-xl mt-1">
-            {{ $message }}
-          </p>
-        @enderror
+        <div class="flex flex-col gap-2 mb-2">
+          <label for="email">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="your@email.com"
+            class="bg-white p-2 border-2 @error('email') border-red-500 @enderror"
+          >
+          @error('email')
+            <p class="text-red-500 text-sm">
+              {{ $message }}
+            </p>
+          @enderror
+        </div>
 
-        <input
-          type="email"
-          name="email"
-          placeholder="your@email.com"
-          class="bg-white p-2 border-2"
-        >
+        <div class="flex flex-col gap-2 mb-4">
+          <label for="password">
+            Senha
+          </label>
 
-        <input
-          type="password"
-          name="password"
-          placeholder="********"
-          class="bg-white p-2 border-2"
-        >
+          <input
+            type="password"
+            name="password"
+            placeholder="********"
+            class="bg-white p-2 border-2 @error('password') border-red-500 @enderror"
+          >
+
+          @error('password')
+            <p class="text-red-500 text-sm">
+              {{ $message }}
+            </p>
+          @enderror
+        </div>
 
         <button
           type="submit"
