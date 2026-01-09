9ce504ff6eba7735acd254f4aa4650e54990b2ae
Victor Alves <viictor.alvess12@gmail.com>
2026-01-08 22:42:32 -0300

    feat: create UpdateHabitRequest to handle habit updates with unique name validation

--- app/Http/Requests/UpdateHabitRequest.php
+++ app/Http/Requests/UpdateHabitRequest.php
@@ -2,35 +2,29 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class HabitRequest extends FormRequest
+class UpdateHabitRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return auth()->check();
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array|string>
-     */
     public function rules(): array
     {
+        $habitId = $this->route('habit')->id;
+
         return [
             'name' => [
                 'required',
                 'max:255',
                 'string',
                 Rule::unique('habits')
-                    ->where(fn (Builder $query) => $query->where('user_id', $this->user()->id)),
+                    ->where(fn (Builder $query) => $query->where('user_id', $this->user()->id))
+                    ->ignore($habitId),
             ],
         ];
     }
